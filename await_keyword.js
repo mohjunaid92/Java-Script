a1dcaba7d44a63eae320d412278c8b6d0965d401
Moh junaid malik <mohdjunaid74560@gmail.com>
2026-01-23 20:18:40 +0530

    changes

---
+++ await_keyword.js
@@ -0,0 +1,55 @@
+function getNum(){
+    return new Promise((resolve, reject) => {
+       setTimeout(()=>{
+         let num = Math.floor(Math.random()*10)+1;
+        console.log(num);
+         resolve("Promise resolved");
+       },1000);
+       
+
+    });
+}
+
+async function demo(){
+     await getNum();
+     await getNum();
+      await getNum();
+}
+demo();
+
+
+// change color function
+let h1 = document.querySelector("h1");
+function changeColor(data, delay){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            h1.style.color = data;
+            console.log(`color change to ${data}`);
+            resolve("change color");
+        },delay);
+    })
+}
+
+async function call(data, delay){
+    await changeColor("blue",1000);
+    await changeColor("red",1000);
+    await changeColor("yellow",1000);
+    await changeColor("pink",1000);
+    await changeColor("orange",1000);
+   return new Promise((resolve, reject) => {
+       setTimeout((()=>{
+        h1.style.color = data;
+        resolve("promise resolved");
+       }),delay);
+   })
+
+}
+call("green",1000)
+.then((result)=>{
+    console.log(result);
+    console.log("promise fullfileed")
+})
+.catch((error)=>{
+    console.log(error);
+    console.log("erroe")
+})
